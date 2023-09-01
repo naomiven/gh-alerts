36a46a1d3c7cab6b8b2cb38f971ec616ca118e1d
naomiven <naomi.mira@gmail.com>
2023-08-31 20:59:11 -0700

    generic header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 
+import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Settings from './pages/Settings/Settings';
 
 function App() {
   return (
-    <Routes>
-      <Route exact path='/' element={<Home />} />
-      <Route path='/settings' element={<Settings />} />
-      {/* <Route path='*' element={<NoPage />} /> */}
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/settings' element={<Settings />} />
+        {/* <Route path='*' element={<NoPage />} /> */}
+      </Routes>
+    </>
   );
 }
 
