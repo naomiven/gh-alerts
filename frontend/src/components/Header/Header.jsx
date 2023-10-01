55e8f7b767bd25e5a50d20ccbc266266b0e71afe
naomiven <naomi.mira@gmail.com>
2023-10-01 16:17:47 -0700

    add links to home and settings

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 const Header = () => {
   return (
@@ -21,8 +22,13 @@ const Header = () => {
             <MenuIcon />
           </IconButton>
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-            Github Alerts
+            <Link href='/' color='inherit' underline='none'>
+              Github Alerts
+            </Link>
           </Typography>
+          <Link href='/settings' color='inherit'>
+            <Button color='inherit'>Settings</Button>
+          </Link>
           <Button color='inherit'>Login</Button>
         </Toolbar>
       </AppBar>
