67894e55f98906d44d25075ed36f4a8853c2ffc5
naomiven <naomi.mira@gmail.com>
2023-12-03 18:34:08 -0800

    static position appbar

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
       <Drawer />
       <Box sx={{ flexGrow: 1 }}>
         <AppBar
-          position='fixed'
+          position='static'
           sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
         >
           <Toolbar>
